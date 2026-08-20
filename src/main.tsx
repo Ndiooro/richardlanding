0042e81929085ba075161a03a7342e1a50eb4ec5
Ndiooro <ndioro003@gmail.com>
2026-08-20 11:12:34 +0000

    ajout de la favicon

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
+  import logoImage from "./imports/logo.png";
   import "./styles/index.css";
+
+  const favicon = document.querySelector<HTMLLinkElement>('link[rel="icon"]') ?? document.createElement("link");
+  favicon.rel = "icon";
+  favicon.type = "image/png";
+  favicon.href = logoImage;
+  document.head.appendChild(favicon);
 
   createRoot(document.getElementById("root")!).render(<App />);
   
